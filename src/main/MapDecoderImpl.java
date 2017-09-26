f8f28fa6b2a9ab546c1d76c13df0989a72ca4730
Shreya Mishra <shreya@Shreyas-MacBook-Pro.local>
2017-09-26 13:49:12 +0200

    Map Decoder

--- src/main/MapDecoderImpl.java
+++ src/main/MapDecoderImpl.java
@@ -29,30 +29,16 @@ public class MapDecoderImpl implements MapDecoder {
     }
 
     public String[] splitByAmpSymbol(String input) {
-
-        if (null == input)
-            return new String[0];
-
-
-       return input.split("&");
-
+        return input.split("&");
     }
 
-    public  String[] splitByEqualsSymbol(String input) {
+    public String[] splitByEqualsSymbol(String input) {
         if (null == input)
             return new String[0];
-
-       /* //TODO : Improve this
-        if ('=' == input.charAt(input.length() - 1)) {
-            String[] emptyVal = new String[2];
-            emptyVal[0] = input.substring(0, input.length() - 1);
-            emptyVal[1] = new String();
-            return emptyVal;
-        }
-*/
 
         String[] split = input.split("=");
 
+        //TODO : Improve this
         if (split.length < 2) {
             if ('=' == input.charAt(input.length() - 1)) {
                 String[] emptyVal = new String[2];
